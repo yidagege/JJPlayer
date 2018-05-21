8b00e26053eae1b88a0860cdb574f825659451d2
zhangyi35 <zhangyi35@jd.com>
2018-05-21 11:21:08 +0800

    cm

--- JJPlay/ViewController.m
+++ JJPlay/ViewController.m
@@ -13,6 +13,7 @@
 @interface ViewController (){
     IJKFFMoviePlayerController *_player;
 }
+@property (nonatomic, strong) NSURL *url;
 
 @end
 
@@ -20,9 +21,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    _player = [[IJKFFMoviePlayerController alloc]initWithContentURLString:@"" withOptions:nil];
-
     
+    
+    NSString * str = [[NSString alloc]initWithFormat:@"%@",[[NSBundle mainBundle]pathForResource:@"playersourse" ofType:@"mov"]];
+    
+    //网络视频
+//        self.url = [NSURL URLWithString:@"https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"];
+//        _player = [[IJKAVMoviePlayerController alloc] initWithContentURL:self.url];
+    
+    //直播视频
+    self.url = [NSURL URLWithString:@"http://live.hkstv.hk.lxdns.com/live/hks/playlist.m3u8"];
+    _player = [[IJKFFMoviePlayerController alloc] initWithContentURL:self.url withOptions:nil];
+
+//    _player = [[IJKFFMoviePlayerController alloc]initWithContentURLString:str withOptions:nil];
+    _player.view.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+    _player.view.frame = self.view.bounds;
+    _player.scalingMode = IJKMPMovieScalingModeAspectFit; //缩放模式
+    _player.shouldAutoplay = YES; //开启自动播放
+    self.view.autoresizesSubviews = YES;
+    [self.view addSubview:_player.view];
+    
+    //准备
+    [_player prepareToPlay];
+    //播放
+    [_player play];
 }
 
 
